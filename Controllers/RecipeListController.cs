62061f857376e0c2563c7912d5a212b0c22dbe36
Jeremy-Thompson <jdt4-@hotmail.com>
2016-05-24 10:56:20 -0700

    Recipe List Dev work

---
+++ Controllers/RecipeListController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace brewstrWebApp.Controllers
+{
+    public class RecipeListController : Controller
+    {
+        //
+        // GET: /RecipeList/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
