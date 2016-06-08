0c049a1db29ceb58933c9879b72c326d0f7879e8
Derek Muxworthy <dmuxwort@sfu.ca>
2016-06-08 08:50:47 -0700

    Populates recipe list programmatically rather than hard coded 	- stores recipe data (string arrays) in the controller with tempdata 	- iteratively populates the list using the string array Fixes the Recipe List build errors

--- Controllers/RecipeListController.cs
+++ Controllers/RecipeListController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using brewstrWebApp.Models.Recipe;
 using System.Data.Sql;
 using System.Data.SqlClient;
 
@@ -16,12 +15,28 @@ namespace brewstrWebApp.Controllers
 
         public ActionResult Index()
         {
+            TempData["imageSource"] = new string[] 
+              { "http://cssdeck.com/uploads/media/items/2/2v3VhAp.png", 
+                "http://cssdeck.com/uploads/media/items/1/1swi3Qy.png", 
+                "http://cssdeck.com/uploads/media/items/6/6f3nXse.png", 
+                "http://cssdeck.com/uploads/media/items/8/8kEc1hS.png",
+                "http://cssdeck.com/uploads/media/items/1/1swi3Qy.png", 
+                "http://cssdeck.com/uploads/media/items/6/6f3nXse.png" };
+
+            TempData["beerName"] = new string[] 
+              { "Oldknow's Nutcracker", 
+                "epic IPA", 
+                "Blonde Pale Ale", 
+                "Nutmeg Stout", 
+                "Island Lager", 
+                "Ray's Raspberry" };
+
             return View();
         }
         [HttpPost]
         public ActionResult Search(string search_str)
         {
-            List<brewstrWebApp.Models.Recipe> Recipe_List = Search_Recipe_By_Name(search_str);
+            brewstrWebApp.Models.RecipeList Recipe_List = Search_Recipe_By_Name(search_str);
             return View();
         }
         public brewstrWebApp.Models.RecipeList Search_Recipe_By_Name(String name)
@@ -45,25 +60,28 @@ namespace brewstrWebApp.Controllers
                 dataReader = command.ExecuteReader();
                 while (dataReader.Read())
                 {
+                   /*
                     r_id = dataReader.GetInt32(0);
                     r_username = dataReader.GetString(1);
                     r_phone_number = dataReader.GetString(2);
                     r_email_address = dataReader.GetString(3);
                     r_password = dataReader.GetString(4);
-
+                    * */
                 }
                 dataReader.Close();
                 command.Dispose();
                 connection.Close();
+                return new brewstrWebApp.Models.RecipeList();
             }
             catch (Exception ex)
             {
                 Console.Write("Can not open connection ! ");
                 Console.Write(ex.Message);
+                return new brewstrWebApp.Models.RecipeList();
             }
 
         }
-  
+
 
     }
 }
