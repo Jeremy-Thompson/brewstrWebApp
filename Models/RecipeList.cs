469abcaa66c4a2d91d3b2ffddd7b37ea0e95ced3
Jeremy-Thompson <jdt4-@hotmail.com>
2016-06-12 11:56:36 -0700

    updated populate recipe list function to hold some test data

--- Models/RecipeList.cs
+++ Models/RecipeList.cs
@@ -17,12 +17,16 @@ namespace brewstrWebApp.Models
         {
             for (int i = 0; i < 10; i++)
             {
-                Recipe_List.Add(new Recipe());
+                Recipe_List.Add(new Recipe(String.Format("Test Name:",i),String.Format("Test Author:",i), DateTime.Now,i,i,i,i,i,i,i,i,i,i));
             }
         }
         public Recipe getRecipeByIndex(int index)
         {
            return Recipe_List.ElementAt(index);
+        }
+        public int getRecipeListSize()
+        {
+            return Recipe_List.Count;
         }
     }
 }
