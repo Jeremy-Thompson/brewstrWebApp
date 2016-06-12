917d5e6e582891d61d7ef0071be6b92e71b86332
Jeremy-Thompson <jdt4-@hotmail.com>
2016-06-12 11:43:58 -0700

    List now populated with default recipes, added function to return recipe model by index

--- Models/RecipeList.cs
+++ Models/RecipeList.cs
@@ -8,5 +8,21 @@ namespace brewstrWebApp.Models
     public class RecipeList
     {
         public List<Recipe> Recipe_List;
+        public RecipeList()
+        {
+            populateListWithBS();
+        }
+
+        public void populateListWithBS()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Recipe_List.Add(new Recipe());
+            }
+        }
+        public Recipe getRecipeByIndex(int index)
+        {
+           return Recipe_List.ElementAt(index);
+        }
     }
 }
